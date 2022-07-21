ae571bbb5bb6ba717948f3f93d8b76ff5399300b
Scarlett <smjeffries514@gmail.com>
2022-07-20 23:15:57 -0600

    Initial Commit

---
+++ esteelauder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  esteelauder
+//
+//  Created by Scarlett  on 7/20/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
